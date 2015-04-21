1c03caca5fdaf2b1d5222a2721472dbe6ad69ca1
Timo Mämecke <timo@maemecke.com>
2015-04-21 23:53:17 +0200

    secrets.js default force push

---
+++ config/secrets.js
@@ -0,0 +1,16 @@
+/**
+ * CAUTION  CAUTION  CAUTION
+ *
+ * Be careful to never ever commit or even push real secret
+ * configurations like keys or passwords here.
+ */
+
+module.exports = {
+
+  development: {
+
+    mongodb: process.env.MONGODB || "mongodb://localhost:27017/sssnap"
+
+  }
+  
+}
